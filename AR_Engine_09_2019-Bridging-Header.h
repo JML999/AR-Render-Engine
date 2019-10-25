14497e68722c40d0dd672ad976d4bfbe992aaaa5
BlueKaneda2020 <Jlee3424@gmail.com>
2019-10-25 15:05:24 -0400

    Add files via upload

---
+++ AR_Engine_09_2019-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  AR_Engine_09_2019-Bridging-Header.h
+//  AR_Engine_09_2019
+//
+//  Created by Justin Lee on 9/17/19.
+//  Copyright © 2019 com.lee. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
